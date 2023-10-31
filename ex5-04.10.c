d0cf8b594b06fc798dc02298337fc86a3f5efaf9
Dayne Pacheco <143220608+daynepacheco9@users.noreply.github.com>
2023-10-31 09:05:30 -0300

    excluindo os códigos a mais

--- ex5-04.10.c
+++
@@ -1,4 +0,0 @@
-/*Faça um programa que leia o tamanho de um vetor de inteiros e reserve
-dinamicamente memoria para esse ´ vetor. Em seguida, leia os elementos
-desse vetor, imprima o vetor lido e mostre o resultado da soma dos números
-ímpares presentes no vetor*/
